f39bb1be7115fba6511340aa439992ead1f91872
newbazz <krutamhathi14@gmail.com>
2019-06-17 16:03:20 +0530

    test changes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -32,7 +32,7 @@ class BasicTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.mimetype, 'application/json')
         body = json.loads(response.data)
-        self.assertEqual(body['status'], 'ok')
+        self.assertEqual(body['name'], 'ok')
 
     def test_menu_empty(self):
         response = self.app.get('/menu', follow_redirects=True)
